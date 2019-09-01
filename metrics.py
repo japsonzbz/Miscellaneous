e2af0cd595e9213206f8dd1279a57deed4672d03
zhangbingzhen <651513423@qq.com>
2019-09-01 22:32:10 +0800

    update model_selection

--- metrics.py
+++ metrics.py
@@ -22,4 +22,4 @@ def mean_absolute_error(y_true, y_predict):
     assert len(y_true) == len(y_predict), \
         "the size of y_true must be equal to the size of y_predict"
 
-    return np.sum(np.absolute(y_predict - y_predict)) / len(y_test)
+    return np.sum(np.absolute(y_predict - y_predict)) / len(y_predict)
